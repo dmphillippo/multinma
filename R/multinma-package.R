afb90bbbadad1befb09d5c41c52902be28d40eda
David Phillippo <david.phillippo@bristol.ac.uk>
2019-08-01 18:39:24 +0100

    Basic description

--- R/multinma-package.R
+++ R/multinma-package.R
@@ -1,6 +1,9 @@
 #' The 'multinma' package.
 #'
-#' @description A DESCRIPTION OF THE PACKAGE
+#' @description This package implements network meta-analysis (NMA) and network
+#'   meta-regression (NMR) models for aggregate data (AgD), individual patient
+#'   data (IPD), and mixtures of both IPD and AgD using multilevel NMR (ML-NMR).
+#'   Models are estimated in a Bayesian framwork using Stan.
 #'
 #' @docType package
 #' @name multinma-package
@@ -10,7 +13,7 @@
 #' @import Rcpp
 #' @importFrom rstan sampling
 #'
-#' @references
-#' Stan Development Team (2019). RStan: the R interface to Stan. R package version 2.19.2. https://mc-stan.org
+#' @references Stan Development Team (2019). RStan: the R interface to Stan. R
+#' package version 2.19.2. https://mc-stan.org
 #'
 NULL
