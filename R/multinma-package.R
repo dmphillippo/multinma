d009de65c9c60970a3e741d2213547a5576d3a46
David Phillippo <david.phillippo@bristol.ac.uk>
2019-08-01 16:14:32 +0100

    Initial commit

---
+++ R/multinma-package.R
@@ -0,0 +1,16 @@
+#' The 'multinma' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name multinma-package
+#' @aliases multinma
+#' @useDynLib multinma, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2019). RStan: the R interface to Stan. R package version 2.19.2. https://mc-stan.org
+#'
+NULL
