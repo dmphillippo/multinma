82d8878a7415cff4a538a220cc2a7399e4535e4d
David Phillippo <david.phillippo@bristol.ac.uk>
2019-08-02 15:06:01 +0100

    Add smoking dataset

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Smoking cessation data
+#'
+#' Data frame containing the results of 24 trials of 4 smoking cessation
+#' treatments \insertCite{Hasselblad1998,TSD4}{multinma}.
+#'
+#' @format A data frame with 50 rows and 5 variables:
+#' \describe{
+#'   \item{studyn}{numeric study ID}
+#'   \item{trtn}{numeric treatment code}
+#'   \item{trtc}{treatment name}
+#'   \item{r}{total number of events}
+#'   \item{n}{total number of individuals}
+#' }
+#'
+#' @references
+#'   \insertAllCited{}
+#'
+
+"smoking"
