866304196fd6cc6686c9a50e867d367cbd7d161f
David Phillippo <david.phillippo@bristol.ac.uk>
2019-08-05 10:50:06 +0100

    Add seealso tags

--- R/nma_data.R
+++ R/nma_data.R
@@ -6,6 +6,9 @@
 #' @return
 #' @export
 #'
+#' @seealso [set_agd_arm()] for arm-based aggregate data, [set_agd_contrast()]
+#'   for contrast-based aggregate data, and [combine()] for combining several data
+#'   sources in one network.
 #' @examples
 set_ipd <- function(data,
                     trt,
@@ -30,6 +33,9 @@ set_ipd <- function(data,
 #' @return
 #' @export
 #'
+#' @seealso [set_ipd()] for individual patient data, [set_agd_contrast()] for
+#'   contrast-based aggregate data, and [combine()] for combining several data
+#'   sources in one network.
 #' @examples
 set_agd_arm <- function(data,
                         trt,
@@ -54,6 +60,9 @@ set_agd_arm <- function(data,
 #' @return
 #' @export
 #'
+#' @seealso [set_ipd()] for individual patient data, [set_agd_arm()] for
+#'   arm-based aggregate data, and [combine()] for combining several data
+#'   sources in one network.
 #' @examples
 set_agd_contrast <- function(data,
                              trt, trt_b,
@@ -76,6 +85,8 @@ set_agd_contrast <- function(data,
 #' @return
 #' @export
 #'
+#' @seealso [set_ipd()], [set_agd_arm()], and [set_agd_contrast()] for defining
+#'   different data sources
 #' @examples
 combine <- function(..., trt_ref) {
   # Check that arguments all inherit from nma_data class
