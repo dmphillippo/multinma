67176f11a6f3148ec584033ebdc84045f620c890
David Phillippo <david.phillippo@bristol.ac.uk>
2019-08-06 12:53:46 +0100

    Fix test

--- tests/testthat/test-data_combine.R
+++ tests/testthat/test-data_combine.R
@@ -2,12 +2,12 @@ library(multinma)
 library(dplyr)
 
 test_that("combine_network error if not passed nma_data objects", {
-  msg <- "Expecting to combine objects of class `nma_data`, created using set_* functions"
+  msg <- "Expecting to combine objects of class `nma_data`, created using set_\\* functions"
 
   expect_error(combine_network(1), msg)
   expect_error(combine_network(1, 2), msg)
-  expect_error(combine_network(1, set_ipd(smoking[NA, ])), msg)
-  expect_error(combine_network(set_ipd(smoking[NA, ]), 1), msg)
+  expect_error(combine_network(1, set_ipd(smoking, studyn, trtc)), msg)
+  expect_error(combine_network(set_ipd(smoking, studyn, trtc), 1), msg)
 })
 
 # Dummy data
