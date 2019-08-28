56398f6760b8db68df96d330609923dcff488218
David Phillippo <david.phillippo@bristol.ac.uk>
2019-08-28 10:16:18 +0100

    Tidy up tests

--- tests/testthat/test-add_integration.R
+++ tests/testthat/test-add_integration.R
@@ -11,16 +11,19 @@ test_that("error on empty network", {
 smknet <- set_agd_arm(smoking, studyn, trtc, r = r, n = n)
 
 test_that("n_int should be a positive integer", {
-  expect_error(add_integration(smknet, n_int = "oh dear"), "should be a positive integer")
-  expect_error(add_integration(smknet, n_int = 1.1), "should be a positive integer")
-  expect_error(add_integration(smknet, n_int = -5), "should be a positive integer")
-  expect_error(add_integration(smknet, n_int = 0), "should be a positive integer")
-  expect_error(add_integration(smknet, n_int = 1:2), "should be a positive integer")
+  m <- "should be a positive integer"
+  expect_error(add_integration(smknet, n_int = "oh dear"), m)
+  expect_error(add_integration(smknet, n_int = 1.1), m)
+  expect_error(add_integration(smknet, n_int = -5), m)
+  expect_error(add_integration(smknet, n_int = 0), m)
+  expect_error(add_integration(smknet, n_int = 1:2), m)
 })
 
 test_that("int_args is named list", {
-  expect_error(add_integration(smknet, int_args = "oh dear"), "should be a named list")
-  expect_error(add_integration(smknet, int_args = 1.1), "should be a named list")
-  expect_error(add_integration(smknet, int_args = NULL), "should be a named list")
-  expect_error(add_integration(smknet, int_args = list(a = 1, 2)), "should be a named list")
+  m <- "should be a named list"
+  expect_error(add_integration(smknet, int_args = "oh dear"), m)
+  expect_error(add_integration(smknet, int_args = 1.1), m)
+  expect_error(add_integration(smknet, int_args = NULL), m)
+  expect_error(add_integration(smknet, int_args = list(a = 1, 2)), m)
+})
 })
