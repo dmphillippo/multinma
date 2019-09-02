338403d437337a5ba8f538583687fd6e75f9669a
David Phillippo <david.phillippo@bristol.ac.uk>
2019-09-02 15:16:57 +0100

    Add prior checks

--- R/priors.R
+++ R/priors.R
@@ -5,6 +5,7 @@
 #' @param mean Prior mean.
 #' @param median Prior median.
 #' @param sd Prior standard deviation.
+#' @param scale Prior scale.
 #' @param df Prior degrees of freedom.
 #' @param rate Prior rate.
 #' @param lower,upper Lower and upper bounds for a uniform prior distribution.
@@ -17,56 +18,65 @@
 #'
 #' @examples
 normal <- function(mean = 0, sd) {
-
+  check_prior_location(mean)
+  check_prior_scale(sd)
 }
 
 #' @rdname priors
 #' @export
 half_normal <- function(sd) {
-
+  check_prior_scale(sd)
 }
 
 #' @rdname priors
 #' @export
-cauchy <- function(median = 0, sd) {
-
+cauchy <- function(median = 0, scale) {
+  check_prior_location(median, type = "median")
+  check_prior_scale(scale, type = "scale")
 }
 
 #' @rdname priors
 #' @export
-half_cauchy <- function(sd) {
-
+half_cauchy <- function(scale) {
+  check_prior_scale(scale, type = "scale")
 }
 
 #' @rdname priors
 #' @export
 student_t <- function(mean = 0, sd, df) {
-
+  check_prior_location(mean)
+  check_prior_scale(sd)
+  check_prior_scale(df, type = "degrees of freedom")
 }
 
 #' @rdname priors
 #' @export
 half_student_t <- function(sd, df) {
-
+  check_prior_scale(sd)
+  check_prior_scale(df, type = "degrees of freedom")
 }
 
 #' @rdname priors
 #' @export
 exponential <- function(rate) {
-
+  check_prior_scale(rate, type = "rate")
 }
 
 #' @rdname priors
 #' @export
 uniform <- function(lower, upper) {
-
+  check_prior_location(lower, type = "lower limit")
+  check_prior_location(lower, type = "upper limit")
 }
 
 # Check functions
 check_prior_location <- function(x, type = "mean") {
-
+  if (!is.numeric(x)) abort(glue::glue("Prior {type} must be numeric."))
+  if (length(x) > 1) abort(glue::glue("Prior {type} must be numeric, length 1."))
 }
 
-check_prior_scale <- function(x, type = "sd") {
-
+check_prior_scale <- function(x, type = "standard deviation") {
+  if (!is.numeric(x)) abort(glue::glue("Prior {type} must be numeric."))
+  if (length(x) > 1) abort(glue::glue("Prior {type} must be numeric, length 1."))
+  if (x <= 0) abort(glue::glue("Prior {type} must be strictly positive."))
 }
