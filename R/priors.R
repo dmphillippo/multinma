9034fb7f181fc7cb5e1252dbefdc70ed29420168
David Phillippo <david.phillippo@bristol.ac.uk>
2019-09-02 11:50:07 +0100

    Add prior skeletons

---
+++ R/priors.R
@@ -0,0 +1,33 @@
+
+normal <- function(mean = 0, sd) {
+
+}
+
+half_normal <- function(sd) {
+
+}
+
+cauchy <- function(median = 0, sd) {
+
+}
+
+student_t <- function(mean = 0, sd, df) {
+
+}
+
+exponential <- function(rate) {
+
+}
+
+uniform <- function(lower, upper) {
+
+}
+
+# Check functions
+check_prior_location <- function(x, type = "mean") {
+
+}
+
+check_prior_scale <- function(x, type = "sd") {
+
+}
