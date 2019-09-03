2f15c3d5dcc4011d3dff1e1a0c1b20141d818b96
David Phillippo <david.phillippo@bristol.ac.uk>
2019-09-03 16:32:28 +0100

    Document nma_prior class

---
+++ R/nma_prior-class.R
@@ -0,0 +1,17 @@
+#' The nma_prior class
+#'
+#' The `nma_prior` class is used to specify prior distributions.
+#'
+#' @rdname nma_prior-class
+#' @name nma_prior-class
+#' @aliases nma_prior
+#'
+#' @details Objects of class `nma_prior` have the following components:
+#'   \describe{
+#'   \item{`dist`}{Distribution name}
+#'   \item{`...`}{Parameters of the distribution}
+#'   }
+#'
+#' The distribution parameters, specified as named components in `...`, match
+#' those in the constructor functions (see [priors]).
+NULL
