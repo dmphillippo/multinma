9d09f06c7601c4bcf406a17c5f171d4e8d87618f
David Phillippo <david.phillippo@bristol.ac.uk>
2019-09-13 14:33:12 +0100

    Refactor priors in terms of location and scale
    Removed uniform() for now...

--- tests/testthat/test-priors.R
+++ tests/testthat/test-priors.R
@@ -1,16 +1,18 @@
 
 test_that("check_prior_location gives suitable errors",{
-  expect_error(check_prior_location("a"), "Prior mean must be numeric")
-  expect_error(check_prior_location(list()), "Prior mean must be numeric")
-  expect_error(check_prior_location(1:2), "Prior mean must be numeric, length 1")
+  m <- "Prior location \\(mean\\) must be numeric"
+  expect_error(check_prior_location("a"), m)
+  expect_error(check_prior_location(list()), m)
+  expect_error(check_prior_location(1:2), m)
   expect_error(check_prior_location("a", type = "median"), "Prior median must be numeric")
 })
 
 test_that("check_prior_scale gives suitable errors",{
-  expect_error(check_prior_scale("a"), "Prior standard deviation must be numeric")
-  expect_error(check_prior_scale(list()), "Prior standard deviation must be numeric")
-  expect_error(check_prior_scale(1:2), "Prior standard deviation must be numeric, length 1")
+  m <- "Prior scale \\(standard deviation\\) must be numeric"
+  expect_error(check_prior_scale("a"), m)
+  expect_error(check_prior_scale(list()), m)
+  expect_error(check_prior_scale(1:2), m)
   expect_error(check_prior_scale("a", type = "variance"), "Prior variance must be numeric")
-  expect_error(check_prior_scale(0), "Prior standard deviation must be strictly positive")
-  expect_error(check_prior_scale(-1), "Prior standard deviation must be strictly positive")
+  expect_error(check_prior_scale(0), "Prior scale \\(standard deviation\\) must be strictly positive")
+  expect_error(check_prior_scale(-1), "Prior scale \\(standard deviation\\) must be strictly positive")
 })
