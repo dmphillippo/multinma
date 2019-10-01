dae4c61c877340f68d97edb8200b0794dff02f2c
David Phillippo <david.phillippo@bristol.ac.uk>
2019-10-01 11:39:20 +0100

    Treat study names as literals in regex for contrast-data baselines

--- R/nma.R
+++ R/nma.R
@@ -326,8 +326,8 @@ nma <- function(network,
 
     # Remove columns for study baselines corresponding to contrast-based studies - not used
     s_contr <- unique(dat_agd_contrast$.study)
-    bl_s_reg <- paste0("^\\.study(", paste0(s_contr, collapse = "|"), ")")
-    bl_cols <- grepl(bl_s_reg, colnames(X_agd_contrast))
+    bl_s_reg <- paste0("^\\.study(\\Q", paste0(s_contr, collapse = "\\E|\\Q"), "\\E)")
+    bl_cols <- grepl(bl_s_reg, colnames(X_agd_contrast), perl = TRUE)
 
     X_agd_contrast <- X_agd_contrast[, !bl_cols]
     if (has_ipd(network)) X_ipd <- X_ipd[, !bl_cols]
