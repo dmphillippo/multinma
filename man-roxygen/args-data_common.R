045a4a56b76bedab4f80ce30c2c02ba5773f671f
David Phillippo <david.phillippo@bristol.ac.uk>
2020-03-16 17:16:16 +0000

    Update trt_ref documentation

--- man-roxygen/args-data_common.R
+++ man-roxygen/args-data_common.R
@@ -2,8 +2,8 @@
 #' @param trt column of `data` specifying treatments, coded using integers,
 #'   strings, or factors
 #' @param trt_ref reference treatment for the network, as a single integer,
-#'   string, or factor. By default takes the first treatment in a "natural" sort
-#'   order.
+#'   string, or factor. If not specified, a reasonable well-connected default
+#'   will be chosen.
 #' @param trt_class column of `data` specifying treatment classes, coded using
 #'   integers, strings, or factors. By default, no classes are specified.
 #' @param study column of `data` specifying the studies, coded using integers,
