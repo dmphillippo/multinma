99fbeb5e9821eef9ce51675a0cb0429feae5deee
David Phillippo <david.phillippo@bristol.ac.uk>
2020-05-18 17:25:26 +0100

    Fix vignettes test path

--- tests/testthat/test-vignettes.R
+++ tests/testthat/test-vignettes.R
@@ -5,45 +5,45 @@ skip_on_cran()
 td <- tempdir()
 
 test_that("Smoking example", {
-  rmarkdown::render("../../vignettes/example_smoking.Rmd", output_dir = td, params = list(run_tests = TRUE))
+  rmarkdown::render("vignettes/example_smoking.Rmd", output_dir = td, params = list(run_tests = TRUE))
 })
 
 test_that("Thrombolytics example", {
-  rmarkdown::render("../../vignettes/example_thrombolytics.Rmd", output_dir = td, params = list(run_tests = TRUE))
+  rmarkdown::render("vignettes/example_thrombolytics.Rmd", output_dir = td, params = list(run_tests = TRUE))
 })
 
 test_that("Blocker example", {
-  rmarkdown::render("../../vignettes/example_blocker.Rmd", output_dir = td, params = list(run_tests = TRUE))
+  rmarkdown::render("vignettes/example_blocker.Rmd", output_dir = td, params = list(run_tests = TRUE))
 })
 
 test_that("Dietary fat example", {
-  rmarkdown::render("../../vignettes/example_dietary_fat.Rmd", output_dir = td, params = list(run_tests = TRUE))
+  rmarkdown::render("vignettes/example_dietary_fat.Rmd", output_dir = td, params = list(run_tests = TRUE))
 })
 
 test_that("Diabetes example", {
-  rmarkdown::render("../../vignettes/example_diabetes.Rmd", output_dir = td, params = list(run_tests = TRUE))
+  rmarkdown::render("vignettes/example_diabetes.Rmd", output_dir = td, params = list(run_tests = TRUE))
 })
 
 test_that("Parkinsons example", {
-  rmarkdown::render("../../vignettes/example_parkinsons.Rmd", output_dir = td, params = list(run_tests = TRUE))
+  rmarkdown::render("vignettes/example_parkinsons.Rmd", output_dir = td, params = list(run_tests = TRUE))
 })
 
 test_that("Transfusion example", {
-  rmarkdown::render("../../vignettes/example_transfusion.Rmd", output_dir = td, params = list(run_tests = TRUE))
+  rmarkdown::render("vignettes/example_transfusion.Rmd", output_dir = td, params = list(run_tests = TRUE))
 })
 
 test_that("Atrial fibrillation example", {
-  rmarkdown::render("../../vignettes/example_atrial_fibrillation.Rmd", output_dir = td, params = list(run_tests = TRUE))
+  rmarkdown::render("vignettes/example_atrial_fibrillation.Rmd", output_dir = td, params = list(run_tests = TRUE))
 })
 
 test_that("Statins example", {
-  rmarkdown::render("../../vignettes/example_statins.Rmd", output_dir = td, params = list(run_tests = TRUE))
+  rmarkdown::render("vignettes/example_statins.Rmd", output_dir = td, params = list(run_tests = TRUE))
 })
 
 test_that("BCG vaccine example", {
-  rmarkdown::render("../../vignettes/example_bcg_vaccine.Rmd", output_dir = td, params = list(run_tests = TRUE))
+  rmarkdown::render("vignettes/example_bcg_vaccine.Rmd", output_dir = td, params = list(run_tests = TRUE))
 })
 
 test_that("Plaque psoriasis example", {
-  rmarkdown::render("../../vignettes/example_plaque_psoriasis.Rmd", output_dir = td, params = list(run_tests = TRUE))
+  rmarkdown::render("vignettes/example_plaque_psoriasis.Rmd", output_dir = td, params = list(run_tests = TRUE))
 })
