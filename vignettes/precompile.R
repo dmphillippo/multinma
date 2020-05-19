dec3267b3c2f6285cc5a30120fea9681798d996a
David Phillippo <david.phillippo@bristol.ac.uk>
2020-05-19 14:59:14 +0100

    Track precompile.R

---
+++ vignettes/precompile.R
@@ -0,0 +1,13 @@
+# Precompile vignettes that run Stan models
+
+rmarkdown::render("vignettes/example_smoking.Rmd")
+rmarkdown::render("vignettes/example_thrombolytics.Rmd")
+rmarkdown::render("vignettes/example_blocker.Rmd")
+rmarkdown::render("vignettes/example_dietary_fat.Rmd")
+rmarkdown::render("vignettes/example_diabetes.Rmd")
+rmarkdown::render("vignettes/example_parkinsons.Rmd")
+rmarkdown::render("vignettes/example_transfusion.Rmd")
+rmarkdown::render("vignettes/example_atrial_fibrillation.Rmd")
+rmarkdown::render("vignettes/example_statins.Rmd")
+rmarkdown::render("vignettes/example_bcg_vaccine.Rmd")
+rmarkdown::render("vignettes/example_plaque_psoriasis.Rmd")
