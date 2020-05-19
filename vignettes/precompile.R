e58edb0253087ddbcc48ba1e88b81effb80d24b1
David Phillippo <david.phillippo@bristol.ac.uk>
2020-05-19 14:59:57 +0100

    Use rprojroot to find vignettes for precompile

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,13 +1,15 @@
 # Precompile vignettes that run Stan models
 
-rmarkdown::render("vignettes/example_smoking.Rmd")
-rmarkdown::render("vignettes/example_thrombolytics.Rmd")
-rmarkdown::render("vignettes/example_blocker.Rmd")
-rmarkdown::render("vignettes/example_dietary_fat.Rmd")
-rmarkdown::render("vignettes/example_diabetes.Rmd")
-rmarkdown::render("vignettes/example_parkinsons.Rmd")
-rmarkdown::render("vignettes/example_transfusion.Rmd")
-rmarkdown::render("vignettes/example_atrial_fibrillation.Rmd")
-rmarkdown::render("vignettes/example_statins.Rmd")
-rmarkdown::render("vignettes/example_bcg_vaccine.Rmd")
-rmarkdown::render("vignettes/example_plaque_psoriasis.Rmd")
+root <- rprojroot::is_r_package
+
+rmarkdown::render(root$find_file("vignettes", "example_smoking.Rmd"))
+rmarkdown::render(root$find_file("vignettes", "example_thrombolytics.Rmd"))
+rmarkdown::render(root$find_file("vignettes", "example_blocker.Rmd"))
+rmarkdown::render(root$find_file("vignettes", "example_dietary_fat.Rmd"))
+rmarkdown::render(root$find_file("vignettes", "example_diabetes.Rmd"))
+rmarkdown::render(root$find_file("vignettes", "example_parkinsons.Rmd"))
+rmarkdown::render(root$find_file("vignettes", "example_transfusion.Rmd"))
+rmarkdown::render(root$find_file("vignettes", "example_atrial_fibrillation.Rmd"))
+rmarkdown::render(root$find_file("vignettes", "example_statins.Rmd"))
+rmarkdown::render(root$find_file("vignettes", "example_bcg_vaccine.Rmd"))
+rmarkdown::render(root$find_file("vignettes", "example_plaque_psoriasis.Rmd"))
