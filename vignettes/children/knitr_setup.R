72989c6e936b3761101235231010e0d5ba0ff615
David Phillippo <david.phillippo@bristol.ac.uk>
2020-11-23 15:33:48 +0000

    Separate knitr_setup for pkgdown vignettes

--- vignettes/children/knitr_setup.R
+++ vignettes/children/knitr_setup.R
@@ -1,23 +1,33 @@
-knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = "#>",
-  dev.args = list(type = "cairo-png", antialias = "subpixel"),
-  fig.width = 6,
-  fig.height = 4.5,
-  fig.align = "center"
-)
-options(width = 100)
 
-# Write vignette details to asis file for precompiled vignettes
-if (!rlang::has_name(rmarkdown::metadata, "vignette")) {
-  .asis <- paste0(gsub("\\.Rmd$", "", knitr::current_input()), ".html.asis")
-  file.create(.asis)
-  cat(
-    paste0("%\\VignetteIndexEntry{", rmarkdown::metadata$title, "}"),
-    "%\\VignetteEngine{R.rsp::asis}",
-    "%\\VignetteEncoding{UTF-8}",
-    sep = "\n",
-    file = .asis)
+if (!pkgdown::in_pkgdown()) { # Setup for vignettes
+  knitr::opts_chunk$set(
+    collapse = TRUE,
+    comment = "#>",
+    dev.args = list(type = "cairo-png", antialias = "subpixel"),
+    fig.width = 6,
+    fig.height = 4.5,
+    fig.align = "center"
+  )
+  options(width = 100)
+
+  # Write vignette details to asis file for precompiled vignettes
+  if (!rlang::has_name(rmarkdown::metadata, "vignette")) {
+    .asis <- paste0(gsub("\\.Rmd$", "", knitr::current_input()), ".html.asis")
+    file.create(.asis)
+    cat(
+      paste0("%\\VignetteIndexEntry{", rmarkdown::metadata$title, "}"),
+      "%\\VignetteEngine{R.rsp::asis}",
+      "%\\VignetteEncoding{UTF-8}",
+      sep = "\n",
+      file = .asis)
+  }
+} else { # Setup for pkgdown articles
+  knitr::opts_chunk$set(
+    collapse = TRUE,
+    comment = "#>",
+    fig.align = "center"
+  )
+  options(width = 100)
 }
 
 nowarn_on_ci <- function(expr, ...) {
