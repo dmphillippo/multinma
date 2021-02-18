9a85d29ca0523d97e4ae1b0807366d7bb8fe868b
David Phillippo <david.phillippo@bristol.ac.uk>
2021-02-18 16:38:57 +0000

    Previous fix broke other checks, keep things simple

--- man-roxygen/ex_smoking_nma_fe_example.R
+++ man-roxygen/ex_smoking_nma_fe_example.R
@@ -1,4 +1,4 @@
 #' @examples \donttest{
 #' # Run smoking FE NMA example if not already available
-#' if (!exists("smk_fit_FE")) example("example_smk_fe"\dontshow{, run.donttest = TRUE})
+#' if (!exists("smk_fit_FE")) example("example_smk_fe", run.donttest = TRUE)
 #' }
