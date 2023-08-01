b32c99f91206cd38c7c373bd15579fa42f5d02d1
David Phillippo <david.phillippo@bristol.ac.uk>
2023-08-01 13:05:58 +0100

    summary_mcmc_array works with completely NA parameters

--- R/nma_summary-class.R
+++ R/nma_summary-class.R
@@ -591,7 +591,12 @@ summary_mcmc_array <- function(x, probs = c(0.025, 0.25, 0.5, 0.75, 0.975)) {
   p_rhat <- apply(x, 3, rstan::Rhat)
   # p_se_mean <- p_sd / sqrt(apply(x, 3, rstan:::ess_mean))
 
-  p_quan <- apply(x, 3, quantile, probs = probs)
+  qt <- function(x, probs, ...) {
+    if (all(is.na(x))) setNames(rlang::rep_along(probs, NA_real_), paste0(probs*100, "%"))
+    else quantile(x, probs = probs, ...)
+  }
+
+  p_quan <- apply(x, 3, qt, probs = probs)
   if (length(probs) == 1) {
     p_quan <- tibble::tibble(!! paste0(probs*100, "%") := p_quan)
   } else {
