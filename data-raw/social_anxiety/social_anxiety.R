f4bdaba35e27d9f82a17dc1f8fc661154996c5b2
Sam Perren <116368778+sjperren@users.noreply.github.com>
2024-10-02 13:20:49 +0100

    Add files via upload

---
+++ data-raw/social_anxiety/social_anxiety.R
@@ -0,0 +1,32 @@
+# Social Anxiety
+
+library(multinma)
+library(dplyr)
+library(tidyr)
+library(readr)
+
+options(mc.cores = parallel::detectCores())
+
+# Read in data
+sa <- read_tsv("social_anxiety.txt")
+sa_trt <- read_tsv("social_anxiety_treatments.txt")
+sa_class <- read_tsv("social_anxiety_classes.txt")
+
+# Tidy data
+social_anxiety <- sa %>%
+  mutate(Var1 = V) %>%
+  pivot_longer(cols = matches("[0-9]+$"),
+               names_to = c(".value", "arm"),
+               names_pattern = "^([a-zA-Z]+)([0-9]+)$") %>%
+  filter(!is.na(t)) %>%
+  transmute(studyn, studyc, trtn = t, y = y, se = sqrt(Var)) %>%
+  # Add in treatment names
+  left_join(sa_trt, by = "trtn") %>%
+  # Add in class details
+  left_join(sa_class, by = "classn")
+
+# Store as standard data.frame
+social_anxiety <- as.data.frame(social_anxiety)
+
+usethis::use_data(social_anxiety)
+
