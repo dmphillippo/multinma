b7a0f9af663577a543d51fda1fba1cedac5b21dc
David Phillippo <david.phillippo@bristol.ac.uk>
2025-03-21 10:29:28 +0000

    More robust tests for class mean and sd names

--- tests/testthat/test-class_effects.R
+++ tests/testthat/test-class_effects.R
@@ -12,28 +12,36 @@ sa_fit_EXclass_RE <- suppressWarnings(nma(sa_net, trt_effects = "random",
                                           iter = 10,
                                           prior_class_sd = normal(0.33,0.1),
                                           class_sd =
-                                            list(`Exercise and SH no support` = c("Exercise promotion", "Self-help no support"),
-                                                 `SSRIs and NSSA` = c("SSRI/SNRI", "NSSA"),
-                                                 `Psychodynamic & Other psychological therapies` = c("Psychodynamic psychotherapy", "Other psychological therapies"))))
+                                            list(`Exercise and SH no support` =
+                                                   c("Exercise promotion", "Self-help no support"),
+                                                 `SSRIs and NSSA` =
+                                                   c("SSRI/SNRI", "NSSA"),
+                                                 `Psychodynamic & Other psychological therapies` =
+                                                   c("Psychodynamic psychotherapy", "Other psychological therapies"))))
 
 test_that("class_mean and class_sd parameters are named correctly", {
   # Extract summaries for class_mean
   cm_summary <- tibble::as_tibble(summary(sa_fit_EXclass_RE, pars = "class_mean"))
   cm_params <- cm_summary$parameter
 
-  expected_possible_cm <- paste0("class_mean[", unique(sa_fit_EXclass_RE$network$classes), "]")
+  # Expected class means - shouldn't have sole-occupancy classes, except if these have shared SDs
+  expected_cm <- setdiff(levels(sa_fit_EXclass_RE$network$classes),
+                         c("Waitlist", "Pill placebo", "Psychological placebo"))
 
-  #Check that every actual parameter name is one of the expected possible names
-  expect_true(all(cm_params %in% expected_possible_cm),
-              info = "Each class_mean parameter should be constructible from the unique classes in the network.")
+  expect_equal(cm_params, paste0("class_mean[", expected_cm, "]"))
 
   # Extract summaries for class_sd
   cs_summary <- tibble::as_tibble(summary(sa_fit_EXclass_RE, pars = "class_sd"))
   cs_params <- cs_summary$parameter
 
-  expected_possible_cs <- paste0("class_sd[", unique(sa_fit_EXclass_RE$network$class_sd), "]")
-
-  #Check that every actual parameter name is one of the expected possible names
-  expect_true(all(cs_params %in% expected_possible_cs),
-              info = "Each class_sd parameter should be constructible from the class_sd list in the network.")
+  # Expected class SDs - shared and named correctly, no sole-occupancy classes
+  expected_cs <- forcats::fct_collapse(sa_fit_EXclass_RE$network$classes,
+                                       `Exercise and SH no support` =
+                                         c("Exercise promotion", "Self-help no support"),
+                                       `SSRIs and NSSA` =
+                                         c("SSRI/SNRI", "NSSA"),
+                                       `Psychodynamic & Other psychological therapies` =
+                                         c("Psychodynamic psychotherapy", "Other psychological therapies")) %>%
+    levels() %>% setdiff(c("Waitlist", "Pill placebo", "Psychological placebo"))
+  expect_equal(cs_params, paste0("class_sd[", expected_cs, "]"))
 })
