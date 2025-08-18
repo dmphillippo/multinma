4cecbaa0a24a96da8629775ed2165b2f2a4c7e70
Sam Perren <ad21883@bristol.ac.uk>
2025-08-18 13:46:14 +0100

    Warning when using Exchangeable class model and ref trt is in class with more than 1 trt, it is removed when running model.

--- R/nma.R
+++ R/nma.R
@@ -731,6 +731,18 @@ nma <- function(network,
   # Notify if network is disconnected
   if (!is_network_connected(network))
     inform("Note: Network is disconnected. See ?is_network_connected for more details.")
+  # Notify if reference treatment is within a class when running the exchangeable class model
+  if (class_effects == "exchangeable" && !is.null(network$classes)) {
+    ref_trt <- levels(network$treatments)[1]
+    ref_class <- levels(network$classes)[1]
+
+    # Count how many treatments share this class
+    n_in_class <- sum(network$classes == ref_class)
+
+    if (n_in_class >= 2) {
+      warn(glue::glue("Note: Reference treatment {ref_trt} has been removed from {ref_class}."))
+    }
+  }
 
   # Get data for design matrices and outcomes
   if (has_ipd(network)) {
