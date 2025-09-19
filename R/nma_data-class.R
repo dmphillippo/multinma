291289f23c4f5230abf96c4a4371f41eeea7902d
Sam Perren <116368778+sjperren@users.noreply.github.com>
2025-09-19 11:05:47 +0100

    ggplot2 update bug fixes (#10)
    * Workaround ggplot 4.0.0 scale names bug
    
    tidyverse/ggplot2#6623
    
    * Update snaps - only slight positioning changes
    
    * Deprecation fixes for ggplot 4.0.0
    
    * More ggplot 4.0.0 deprecations
    
    ---------
    
    Co-authored-by: David Phillippo <david.phillippo@bristol.ac.uk>

--- R/nma_data-class.R
+++ R/nma_data-class.R
@@ -947,7 +947,7 @@ plot.nma_data <- function(x, ..., layout, circular,
   }
 
   if (show_trt_class) {
-    g <- g + ggplot2::labs(fill = "Treatment Class", colour = "Treatment Class")
+    g <- g + ggplot2::scale_fill_discrete(name = "Treatment Class", aesthetics = c("fill", "colour"))
   }
 
   if (show_single_arm && !is.null(sa_nodes)) {
