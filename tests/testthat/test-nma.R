ebd6a6ba7ed21768f3df89a2101e06b321f0e40a
Sam Perren <ad21883@bristol.ac.uk>
2025-09-22 16:14:56 +0100

    placed data for test inside the test itself

--- tests/testthat/test-nma.R
+++ tests/testthat/test-nma.R
@@ -492,77 +492,79 @@ test_that("rstan R-hat and ESS warnings are captured correctly", {
     "Tail Effective Sample(s?) Size \\(ESS\\) is too low")
 })
 
-pso_ipd <- plaque_psoriasis_ipd %>%
-  mutate(
-    # Variable transformations
-    bsa = bsa / 100,
-    weight = weight / 10,
-    durnpso = durnpso / 10,
-    prevsys = as.numeric(prevsys),
-    psa = as.numeric(psa),
-    # Treatment classes
-    trtclass = case_when(trtn == 1 ~ "Placebo",
-                         trtn %in% c(2, 3, 5, 6) ~ "IL-17 blocker",
-                         trtn == 4 ~ "TNFa blocker",
-                         trtn == 7 ~ "IL-12/23 blocker"),
-    # Check complete cases for covariates of interest
-    is_complete = complete.cases(durnpso, prevsys, bsa, weight, psa)
-  ) %>%
-  arrange(studyc, trtn)
 
-# AgD studies
-pso_agd <- plaque_psoriasis_agd %>%
-  mutate(
-    # Variable transformations
-    bsa_mean = bsa_mean / 100,
-    bsa_sd = bsa_sd / 100,
-    weight_mean = weight_mean / 10,
-    weight_sd = weight_sd / 10,
-    durnpso_mean = durnpso_mean / 10,
-    durnpso_sd = durnpso_sd / 10,
-    prevsys = prevsys / 100,
-    psa = psa / 100,
-    # Treatment classes
-    trtclass = case_when(trtn == 1 ~ "Placebo",
-                         trtn %in% c(2, 3, 5, 6) ~ "IL-17 blocker",
-                         trtn == 4 ~ "TNFa blocker",
-                         trtn == 7 ~ "IL-12/23 blocker")
-  ) %>%
-  arrange(studyc, trtn)
-
-
-# Missing Data
-pso_ipd %>%
-  group_by(studyc) %>%
-  summarise(n_total = n(),
-            n_missing = sum(!is_complete),
-            pct_missing = mean(!is_complete) * 100)
-
-pso_ipd <- filter(pso_ipd, is_complete)
-
-# Creating the FULL network
-pso_net <- combine_network(
-  set_ipd(pso_ipd,
-          study = studyc,
-          trt = trtc,
-          r = multi(r0 = 1,
-                    PASI75 = pasi75,
-                    PASI90 = pasi90,
-                    PASI100 = pasi100,
-                    type = "ordered", inclusive = TRUE),
-          trt_class = trtclass),
-  set_agd_arm(pso_agd,
-              study = studyc,
-              trt = trtc,
-              r = multi(r0 = pasi75_n,
-                        PASI75 = pasi75_r,
-                        PASI90 = pasi90_r,
-                        PASI100 = pasi100_r,
-                        type = "ordered", inclusive = TRUE),
-              trt_class = trtclass)
-)
 
 test_that("con() recieves correct arguments", {
+  pso_ipd <- plaque_psoriasis_ipd %>%
+    mutate(
+      # Variable transformations
+      bsa = bsa / 100,
+      weight = weight / 10,
+      durnpso = durnpso / 10,
+      prevsys = as.numeric(prevsys),
+      psa = as.numeric(psa),
+      # Treatment classes
+      trtclass = case_when(trtn == 1 ~ "Placebo",
+                           trtn %in% c(2, 3, 5, 6) ~ "IL-17 blocker",
+                           trtn == 4 ~ "TNFa blocker",
+                           trtn == 7 ~ "IL-12/23 blocker"),
+      # Check complete cases for covariates of interest
+      is_complete = complete.cases(durnpso, prevsys, bsa, weight, psa)
+    ) %>%
+    arrange(studyc, trtn)
+
+  # AgD studies
+  pso_agd <- plaque_psoriasis_agd %>%
+    mutate(
+      # Variable transformations
+      bsa_mean = bsa_mean / 100,
+      bsa_sd = bsa_sd / 100,
+      weight_mean = weight_mean / 10,
+      weight_sd = weight_sd / 10,
+      durnpso_mean = durnpso_mean / 10,
+      durnpso_sd = durnpso_sd / 10,
+      prevsys = prevsys / 100,
+      psa = psa / 100,
+      # Treatment classes
+      trtclass = case_when(trtn == 1 ~ "Placebo",
+                           trtn %in% c(2, 3, 5, 6) ~ "IL-17 blocker",
+                           trtn == 4 ~ "TNFa blocker",
+                           trtn == 7 ~ "IL-12/23 blocker")
+    ) %>%
+    arrange(studyc, trtn)
+
+
+  # Missing Data
+  pso_ipd %>%
+    group_by(studyc) %>%
+    summarise(n_total = n(),
+              n_missing = sum(!is_complete),
+              pct_missing = mean(!is_complete) * 100)
+
+  pso_ipd <- filter(pso_ipd, is_complete)
+
+  # Creating the FULL network
+  pso_net <- combine_network(
+    set_ipd(pso_ipd,
+            study = studyc,
+            trt = trtc,
+            r = multi(r0 = 1,
+                      PASI75 = pasi75,
+                      PASI90 = pasi90,
+                      PASI100 = pasi100,
+                      type = "ordered", inclusive = TRUE),
+            trt_class = trtclass),
+    set_agd_arm(pso_agd,
+                study = studyc,
+                trt = trtc,
+                r = multi(r0 = pasi75_n,
+                          PASI75 = pasi75_r,
+                          PASI90 = pasi90_r,
+                          PASI100 = pasi100_r,
+                          type = "ordered", inclusive = TRUE),
+                trt_class = trtclass)
+  )
+
   expect_error(nma(pso_net, connect_baseline = con(type = "rando", studies = c("FIXTURE", "FEATURE"), baseline_prior = normal(0,10))), "type must equal 'fixed' or 'random'.")
   expect_error(nma(pso_net, connect_baseline = con(type = 1, studies = c("FIXTURE", "FEATURE"), baseline_prior = normal(0,10))), "type must equal 'fixed' or 'random'.")
   expect_error(nma(pso_net, connect_baseline = list(con(type = "rando", studies = c("FIXTURE", "FEATURE"), baseline_prior = normal(0,10)), con(type = "random", studies = c("JUNCTURE"), baseline_prior = normal(0,10)))), "type must equal 'fixed' or 'random'.")
