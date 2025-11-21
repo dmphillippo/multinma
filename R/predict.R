708598ab5d6e1addb3f8b467b35a2281faa6a0c6
David Phillippo <david.phillippo@bristol.ac.uk>
2025-11-21 19:45:57 +0000

    Fix generalised gamma predictions with aux_regression
    Fixes #62

--- R/predict.R
+++ R/predict.R
@@ -2502,9 +2502,11 @@ make_aux_predict <- function(aux, beta_aux, X_aux, likelihood) {
     out <- aperm(apply(lscoef_reg, 1:2, softmax), c(2, 3, 1))
 
   } else if (likelihood == "gengamma") {
+
+    nX <- ncol(X_aux)
     out <- aux
-    out[ , , 1] <- exp(log(aux[ , , 1, drop = FALSE]) + tcrossprod_mcmc_array(beta_aux[ , , 1, drop = FALSE], X_aux))
-    out[ , , 2] <- exp(log(aux[ , , 2, drop = FALSE]) + tcrossprod_mcmc_array(beta_aux[ , , 2, drop = FALSE], X_aux))
+    out[ , , 1] <- exp(log(aux[ , , 1, drop = FALSE]) + tcrossprod_mcmc_array(beta_aux[ , , 1:nX, drop = FALSE], X_aux))
+    out[ , , 2] <- exp(log(aux[ , , 2, drop = FALSE]) + tcrossprod_mcmc_array(beta_aux[ , , (nX+1):(2*nX), drop = FALSE], X_aux))
 
   } else {
     out <- exp(log(aux) + tcrossprod_mcmc_array(beta_aux, X_aux))
