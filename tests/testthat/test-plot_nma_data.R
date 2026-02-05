68bf8c883a920e76ecd3490af8452f34c4d77093
David Phillippo <david.phillippo@bristol.ac.uk>
2026-02-05 14:20:11 +0000

    dplyr 1.2.0 deprecations

--- tests/testthat/test-plot_nma_data.R
+++ tests/testthat/test-plot_nma_data.R
@@ -287,13 +287,13 @@ test_that("Transfusion", {
 
 test_that("NDMM", {
 
-  ndmm_ipd$trtclass <- case_match(ndmm_ipd$trtf,
-                                  "Pbo" ~ "Placebo",
-                                  c("Len", "Thal") ~ "Active")
+  ndmm_ipd$trtclass <- forcats::fct_collapse(ndmm_ipd$trtf,
+                                             Placebo = "Pbo",
+                                             Active = c("Len", "Thal"))
 
-  ndmm_agd$trtclass <- case_match(ndmm_agd$trtf,
-                                  "Pbo" ~ "Placebo",
-                                  c("Len", "Thal") ~ "Active")
+  ndmm_agd$trtclass <- forcats::fct_collapse(ndmm_agd$trtf,
+                                             Placebo = "Pbo",
+                                             Active = c("Len", "Thal"))
 
   ndmm_net <- combine_network(
     set_ipd(ndmm_ipd,
