22f200a9162058f81724436268642f883d1233e5
David Phillippo <david.phillippo@bristol.ac.uk>
2026-07-29 18:25:08 +0100

    Add mixed data type to palette

--- R/nma_data-class.R
+++ R/nma_data-class.R
@@ -948,7 +948,7 @@ plot.nma_data <- function(x, ..., layout, circular,
   }
 
   g <- g +
-    ggraph::scale_edge_colour_manual("Data", values = c(AgD = "#113259", IPD = "#55A480"),
+    ggraph::scale_edge_colour_manual("Data", values = c(AgD = "#113259", IPD = "#55A480", Mixed = "#E5A93C"),
                                      guide = if (dat_mixed) "legend" else "none") +
     ggraph::theme_graph(base_family = "") +
     ggplot2::coord_fixed(clip = "off")
